6b2432801994f8cc17cdaae712b39e8c49448daa
brandon57 <bmsitarz@gmail.com>
2023-10-26 19:02:02 -0500

    Improved Client application
    Can now type in the IP address and port number for the server you want
    to connect to
    
    Also simplified a lot of code

--- Client/src/clients/ClientSide.java
+++ Client/src/clients/ClientSide.java
@@ -2,111 +2,134 @@ package clients;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class ClientSide {
 
 	private static Socket client = null;
+	private static Scanner input = new Scanner(System.in);
+	private static DataOutputStream toServer = null;
+	private static BufferedReader fromServer = null;
 	
 	public static void main(String[] args) throws Exception {
 		String textFromServer = "";
 		//Socket client = null;
 		String response = "";
-		DataOutputStream toServer = null;
-		BufferedReader fromServer = null;
+
+		boolean valid_input = false;
 		
-		Scanner input = new Scanner(System.in);
 		
-		System.out.println("What do you want to do?\n1. Connect to GoFundMe\n2. Exit");
+		//System.out.println("What do you want to do?\n1. Connect a website\n2. Exit");
+		
+		//InetAddress
 		
 		//Asks the user if they want to connect or not
-		while(true)
+		while(valid_input == false)
 		{
-			response = input.nextLine();
-			if(response.equals("1") || response.equalsIgnoreCase("connect"))
+			System.out.println("What do you want to do?\n1. Connect to a website\n2. Exit");
+			response = input.nextLine().toLowerCase();
+			switch(response)
 			{
-				break;
+				case "connect":
+				case "1":
+					connect();
+					valid_input = true;
+					break;
+				case "exit":
+				case "2":
+					close();
+					break;
+				default:
+					System.out.println("Your input is not valid\nTry again");
+					break;
 			}
-			else if(response.equals("2") || response.equalsIgnoreCase("Exit"))
-			{
-				input.close();
-				close();
-			}
-			else
-			{
-				System.out.println("Your input is not valid\nWant to try again?\n1. Yes\n2. No");
-				response = input.nextLine();
-				while(true)
-				{
-					if(response.equals("1") || response.equalsIgnoreCase("yes"))
-					{
-						System.out.println("What do you want to do?\n1. Connect to website\n2. Exit");
-						break;
-					}
-					else if(response.equals("2") || response.equalsIgnoreCase("no"))
-					{
-						input.close();
-						close();
-					}
-					else
-					{
-						System.out.println("Not a valid option\nYou want to try again?\n1. Yes\n2. No");
-						response = input.nextLine();
-					}
-				}
-			}
+			
+//			if(response.equals("1") || response.equalsIgnoreCase("connect"))
+//			{
+//				break;
+//			}
+//			else if(response.equals("2") || response.equalsIgnoreCase("Exit"))
+//			{
+//				input.close();
+//				close();
+//			}
+//			else
+//			{
+//				System.out.println("Your input is not valid\nWant to try again?\n1. Yes\n2. No");
+//				response = input.nextLine();
+//				while(true)
+//				{
+//					if(response.equals("1") || response.equalsIgnoreCase("yes"))
+//					{
+//						System.out.println("What do you want to do?\n1. Connect to a website\n2. Exit");
+//						break;
+//					}
+//					else if(response.equals("2") || response.equalsIgnoreCase("no"))
+//					{
+//						input.close();
+//						close();
+//					}
+//					else
+//					{
+//						System.out.println("Not a valid option\nYou want to try again?\n1. Yes\n2. No");
+//						response = input.nextLine();
+//					}
+//				}
+//			}
 		}
 		
 		//Opens socket
-		while(true)
-		{
-			try
-			{
-				client = new Socket("localhost", 6789);
-				fromServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				break;
-				//client.close();
-			}
-			catch(Exception e)
-			{
-				System.out.println("Couldn't connect\nWant to try again?\n1. Yes\n2. No");
-				while(true)
-				{
-					response = input.nextLine();
-					if(response.equals("1") || response.equalsIgnoreCase("yes"))
-					{
-						break;
-					}
-					else if(response.equals("2") || response.equalsIgnoreCase("no"))
-					{
-						input.close();
-						close();
-					}
-					else
-					{
-						System.out.println("Your input is not valid\nWant to try again?\n1. Yes\n2. No");
-						response = input.nextLine();
-						while(true)
-						{
-							if(response.equals("1") || response.equalsIgnoreCase("yes"))
-							{
-								System.out.println("Want to try and connect again?\n1. Yes\n2. No");
-								break;
-							}
-							else if(response.equals("2") || response.equalsIgnoreCase("no"))
-							{
-								input.close();
-								close();
-							}
-							else
-							{
-								System.out.println("Not a valid option\nYou want to try again?\n1. Yes\n2. No");
-								response = input.nextLine();
-							}
-						}
-					}
-				}
-			}
-		}
+//		while(true)
+//		{
+//			try
+//			{
+//				client = new Socket("localhost", 6789);
+//				
+//				break;
+//				//client.close();
+//			}
+//			catch(Exception e)
+//			{
+//				System.out.println("Couldn't connect\nWant to try again?\n1. Yes\n2. No");
+//				while(true)
+//				{
+//					response = input.nextLine();
+//					if(response.equals("1") || response.equalsIgnoreCase("yes"))
+//					{
+//						break;
+//					}
+//					else if(response.equals("2") || response.equalsIgnoreCase("no"))
+//					{
+//						input.close();
+//						close();
+//					}
+//					else
+//					{
+//						System.out.println("Your input is not valid\nWant to try again?\n1. Yes\n2. No");
+//						response = input.nextLine();
+//						while(true)
+//						{
+//							if(response.equals("1") || response.equalsIgnoreCase("yes"))
+//							{
+//								System.out.println("Want to try and connect again?\n1. Yes\n2. No");
+//								break;
+//							}
+//							else if(response.equals("2") || response.equalsIgnoreCase("no"))
+//							{
+//								input.close();
+//								close();
+//							}
+//							else
+//							{
+//								System.out.println("Not a valid option\nYou want to try again?\n1. Yes\n2. No");
+//								response = input.nextLine();
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+		fromServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		toServer = new DataOutputStream(client.getOutputStream());
 		
 		//The part where the User actually interacts with the Website
@@ -138,7 +161,74 @@ public class ClientSide {
 		}
 	}
 	
+	//This connects you to a website
+	private static void connect() throws Exception
+	{
+		String IP = null;
+		String port = null;
+		while(true)
+		{
+			System.out.println("Where do you want to connect to? Type the ip address\nYour input should look like \"0-255.0-255.0-255.0-255\"\nYou can exit if you want");
+			IP = input.nextLine();
+			if(validIP(IP) == true)
+			{
+				break;
+			}
+			else if(IP.equalsIgnoreCase("exit"))
+			{
+				close();
+			}
+			else
+			{
+				System.out.println("That IP address is not valid. Try again");
+			}
+		}
+		while(true)
+		{
+			System.out.println("What is the port number of the server? Your input should be between 0-65535\nYou can exit if you want");
+			port = input.nextLine();
+			if(validPort(port) == true)
+			{
+				break;
+			}
+			else if(port.equalsIgnoreCase("exit"))
+			{
+				close();
+			}
+			else
+			{
+				System.out.println("That port number is not valid");
+			}
+		}
+		client = new Socket(IP, Integer.valueOf(port));
+	}
 	
+	private static boolean validPort(String port)
+	{
+		Integer temp = 0;
+		try
+		{
+			temp = Integer.valueOf(port);
+			if(temp >= 0 && temp <= 65535)
+			{
+				return true;
+			}
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		return false;
+	}
+	
+	private static boolean validIP(String IP)
+	{
+		String pattern = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+		String IP_pattern = pattern + "." + pattern + "." + pattern + "." + pattern;
+		Pattern temp = Pattern.compile(IP_pattern);
+		Matcher check = temp.matcher(IP);
+		return check.matches();
+	}
 	
 	//Stops the program
 	private static void close() throws Exception
@@ -146,6 +236,10 @@ public class ClientSide {
 		if(client != null)
 		{
 			client.close();
+		}
+		if(input != null)
+		{
+			input.close();
 		}
 		System.out.println("Closing...");
 		System.exit(0);
